9d19b685e645aa497c13935c47e389d37f9ddbd4
malmen237 <linda.malm@eyevinn.se>
2024-08-30 14:41:14 +0200

    feat: updated server-fetch to be put, not possible to delete

--- src/hooks/sources/useDeleteSource.tsx
+++ src/hooks/sources/useDeleteSource.tsx
@@ -9,18 +9,22 @@ export function useDeleteSource(source: SourceWithId | null) {
     if (source && source.status === 'gone') {
       setLoading(true);
       setDeleteComplete(false);
-      // Source to be deleted:
-      console.log('source._id', source);
+
       fetch(`/api/manager/inventory/${source._id}`, {
-        method: 'DELETE',
+        method: 'PUT',
         // TODO: Implement api key
         headers: [['x-api-key', `Bearer apisecretkey`]]
       })
         .then((response) => {
-          if (response.ok) {
+          if (!response.ok) {
             setLoading(false);
             setDeleteComplete(true);
+            return response.text().then((message) => {
+              throw new Error(`Error ${response.status}: ${message}`);
+            });
           }
+          setLoading(false);
+          setDeleteComplete(true);
         })
         .catch((e) => {
           console.log(`Failed to delete source-item: ${e}`);
