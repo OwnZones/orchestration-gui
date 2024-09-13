f9fc4af4a04ad489a1336f1a19ef3054d210ad7b
Saelmala <sandra.larsson@eyevinn.se>
2024-09-13 09:13:05 +0200

    fix: add back previous functionality and empty slot card

--- src/components/sourceCards/SourceCards.tsx
+++ src/components/sourceCards/SourceCards.tsx
@@ -5,6 +5,7 @@ import { Production } from '../../interfaces/production';
 import DragItem from '../dragElement/DragItem';
 import SourceCard from '../sourceCard/SourceCard';
 import { ISource, useDragableItems } from '../../hooks/useDragableItems';
+import { EmptySlotCard } from '../emptySlotCard/EmptySlotCard';
 export default function SourceCards({
   productionSetup,
   updateProduction,
@@ -23,42 +24,104 @@ export default function SourceCards({
     return 'src' in source;
   };
 
-  const gridItems = items.map((source) => {
-    const id = source._id ? source._id : '';
-    const isSource = isISource(source);
-    return (
-      <DragItem
-        key={id === typeof String ? id : id.toString()}
-        id={id}
-        onMoveItem={moveItem}
-        previousOrder={productionSetup.sources}
-        currentOrder={items as SourceReference[]}
-        productionSetup={productionSetup}
-        updateProduction={updateProduction}
-        selectingText={selectingText}
-      >
-        {isSource ? (
-          <SourceCard
-            source={source}
-            label={source.label}
-            src={source.src}
-            onSourceUpdate={onSourceUpdate}
-            onSourceRemoval={onSourceRemoval}
-            onSelectingText={(isSelecting) => setSelectingText(isSelecting)}
-            type={'ingest_source'}
-          />
-        ) : (
-          <SourceCard
-            sourceRef={source}
-            label={source.label}
-            onSourceUpdate={onSourceUpdate}
-            onSourceRemoval={onSourceRemoval}
-            onSelectingText={(isSelecting) => setSelectingText(isSelecting)}
-            type={source.type}
-          />
-        )}
-      </DragItem>
-    );
-  });
+  const gridItems: React.JSX.Element[] = [];
+  let tempItems = [...items];
+  let firstEmptySlot = items.length + 1;
+
+  if (!items || items.length === 0) return null;
+  for (let i = 0; i < items[items.length - 1].input_slot; i++) {
+    if (!items.some((source) => source.input_slot === i + 1)) {
+      firstEmptySlot = i + 1;
+      break;
+    }
+  }
+
+  for (let i = 0; i < items[items.length - 1].input_slot; i++) {
+    tempItems.every((source) => {
+      const id = source._id ? source._id : '';
+      const isSource = isISource(source);
+      if (source.input_slot === i + 1) {
+        tempItems = tempItems.filter((i) => i._id !== source._id);
+        if (!productionSetup.isActive) {
+          gridItems.push(
+            <DragItem
+              key={id === typeof String ? id : id.toString()}
+              id={id}
+              onMoveItem={moveItem}
+              previousOrder={productionSetup.sources}
+              currentOrder={items as SourceReference[]}
+              productionSetup={productionSetup}
+              updateProduction={updateProduction}
+              selectingText={selectingText}
+            >
+              {isSource ? (
+                <SourceCard
+                  source={source}
+                  label={source.label}
+                  src={source.src}
+                  onSourceUpdate={onSourceUpdate}
+                  onSourceRemoval={onSourceRemoval}
+                  onSelectingText={(isSelecting) =>
+                    setSelectingText(isSelecting)
+                  }
+                  type={'ingest_source'}
+                />
+              ) : (
+                <SourceCard
+                  sourceRef={source}
+                  label={source.label}
+                  onSourceUpdate={onSourceUpdate}
+                  onSourceRemoval={onSourceRemoval}
+                  onSelectingText={(isSelecting) =>
+                    setSelectingText(isSelecting)
+                  }
+                  type={source.type}
+                />
+              )}
+            </DragItem>
+          );
+        } else {
+          isSource
+            ? gridItems.push(
+                <SourceCard
+                  source={source}
+                  label={source.label}
+                  src={source.src}
+                  onSourceUpdate={onSourceUpdate}
+                  onSourceRemoval={onSourceRemoval}
+                  onSelectingText={(isSelecting) =>
+                    setSelectingText(isSelecting)
+                  }
+                  type={'ingest_source'}
+                />
+              )
+            : gridItems.push(
+                <SourceCard
+                  sourceRef={source}
+                  label={source.label}
+                  onSourceUpdate={onSourceUpdate}
+                  onSourceRemoval={onSourceRemoval}
+                  onSelectingText={(isSelecting) =>
+                    setSelectingText(isSelecting)
+                  }
+                  type={source.type}
+                />
+              );
+        }
+        return false;
+      } else {
+        if (productionSetup.isActive) {
+          gridItems.push(
+            <EmptySlotCard
+              key={i}
+              inputSlot={i + 1}
+              isFirstEmptySlot={firstEmptySlot === i + 1}
+            />
+          );
+        }
+        return false;
+      }
+    });
+  }
   return <>{gridItems}</>;
 }
